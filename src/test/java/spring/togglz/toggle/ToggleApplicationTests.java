c3bad069cf463ccf7015239c17d559a9ee09a503
piyushbhargava1412 <piyush.bhargava@thoughtworks.com>
2025-02-24 17:45:40 +0530

    New togglz demo repo

---
+++ src/test/java/spring/togglz/toggle/ToggleApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.togglz.toggle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToggleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
